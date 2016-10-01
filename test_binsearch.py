9602ca8beeb459280a80753ce19c081a16f3d77e
vol <xiabingfeng@gmail.com>
2016-09-30 21:19:55 -0400

    fix bugs in test_b.py

--- test_binsearch.py
+++ test_binsearch.py
@@ -9,7 +9,7 @@ def test_bad_input_results():
         binary_search([(1,2),3,4,5], 2)
     with raises(ValueError):
         binary_search([5,4,3,2,1], 2)
-    with raises(ValueError):
+    with raises(TypeError):
         binary_search([1,2,3,4,5], 2, 0.5, 1.4)
     with raises(ValueError):
         binary_search([1,2,3,4,5], 2, 1, 100)
